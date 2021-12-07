781cd6821061e14da1ce04b37b2412da10e6cc66
Kim Yong Hoon <rkdntm1@gmail.com>
2021-12-07 10:13:09 +0900

    검색기능 추가(검색시 검색 텍스트 파라미터 유지)

--- src/main/java/com/example/demo/repository/BoardRepository.java
+++ src/main/java/com/example/demo/repository/BoardRepository.java
@@ -2,6 +2,8 @@ package com.example.demo.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.model.Board;	
@@ -10,5 +12,7 @@ public interface BoardRepository extends JpaRepository<Board, Long>{
 	
 	List<Board> findByTitle(String title);
 	List<Board> findByTitleOrContent(String title, String content);
+	
+	Page<Board> findByTitleContainingOrContentContaining(String title, String content, Pageable pageable);
 
 }
